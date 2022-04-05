5025a3506f1b859abf6c85959ee4946ba757f067
Vasenin-Sashko <vasenin.as@yandex.ru>
2022-04-05 22:23:39 +0300

    Реализована программа для добавления файла на яндекс диск с пк

---
+++ ya_disk.py
@@ -0,0 +1,41 @@
+from pprint import pprint
+
+import requests
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, disk_file_path, file_path: str):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(file_path, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+        
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        # Функция может ничего не возвращать
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    disk_file_path = 'Yandex_file\Text.txt'
+    path_to_file = "Text.txt"
+    token = ''
+    uploader = YaUploader(token)
+    result = uploader.upload(disk_file_path, path_to_file)
